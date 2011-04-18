39c42128197da9ab016b7519878c862eb59db4b6
Brooks Boyd <boydb@midnightdesign.ws>
2011-04-18 11:20:53 -0500

    generate blank level.dat and chunk NBTs

--- nbt/mapbuilder/utilities.py
+++ nbt/mapbuilder/utilities.py
@@ -1,4 +1,7 @@
-import math
+import array, math, random, time
+from nbt import *
+from struct import pack
+from StringIO import StringIO
 
 def ByteToHex(byteStr):
 	return "".join(["%02X " % ord(x) for x in byteStr]).strip()
@@ -22,6 +25,97 @@ def blank_region(filename):
 	f.seek(8191)
 	f.write(chr(0))
 	f.close()
+
+def blank_chunk_nbt(x=0,z=0):
+	#http://www.minecraftwiki.net/wiki/Alpha_Level_Format/Chunk_File_Format
+	chunk = NBTFile() # Blank NBT
+
+	blocklight_str = pack(">i", 16384)+array.array('B', [0]*16384).tostring()
+	blocks_str = pack(">i", 32768)+array.array('B', [0]*32768).tostring()
+	data_str = pack(">i", 16384)+array.array('B', [0]*16384).tostring()
+	skylight_str = pack(">i", 16384)+array.array('B', [255]*16384).tostring()
+	heightmap_str = pack(">i", 256)+array.array('B', [0]*255).tostring()
+
+	level = TAG_Compound()
+	level.name = "Level"
+	level.tags.extend([
+		TAG_Byte(name="TerrainPopulated", value=1),
+		TAG_Int(name="xPos", value=x),
+		TAG_Int(name="zPos", value=z),
+		TAG_Long(name="LastUpdate", value=0),
+		TAG_Byte_Array(name="BlockLight", buffer=StringIO(blocklight_str)),
+		TAG_Byte_Array(name="Blocks", buffer=StringIO(blocks_str)),
+		TAG_Byte_Array(name="Data", buffer=StringIO(data_str)),
+		TAG_Byte_Array(name="SkyLight", buffer=StringIO(skylight_str)),
+		TAG_Byte_Array(name="HeightMap", buffer=StringIO(heightmap_str)),
+		TAG_List(name="Entities", type=TAG_Compound),
+		TAG_List(name="TileEntities", type=TAG_Compound)
+	])
+	chunk.tags.append(level)
+	return chunk
+
+def blank_level_nbt(name="Testing", spawn_x=0, spawn_y=64, spawn_z=0, version=19132):
+	# http://www.minecraftwiki.net/wiki/Alpha_Level_Format#level.dat_Format
+	level = NBTFile() # Blank NBT
+	data = TAG_Compound()
+	data.name = "Data"
+	data.tags.extend([
+		TAG_Long(name="Time", value=1),
+		TAG_Long(name="LastPlayed", value=int(time.time()*1000)),
+		TAG_Int(name="SpawnX", value=spawn_x),
+		TAG_Int(name="SpawnY", value=spawn_y),
+		TAG_Int(name="SpawnZ", value=spawn_z),
+		TAG_Long(name="SizeOnDisk", value=0),
+		TAG_Long(name="RandomSeed", value=random.randrange(1,9999999999)),
+		TAG_Int(name="version", value=version),
+		TAG_String(name="LevelName", value=name)
+	])
+	player = TAG_Compound()
+	player.name = "Player"
+	player.tags.extend([
+		TAG_Int(name="Score", value=0),
+		TAG_Int(name="Dimension", value=0),
+		TAG_Short(name="Air", value=300),
+		TAG_Short(name="AttackTime", value=0),
+		TAG_Short(name="DeathTime", value=0),
+		TAG_Short(name="Fire", value=-20),
+		TAG_Short(name="Health", value=20),
+		TAG_Short(name="HurtTime", value=0),
+		TAG_Float(name="FallDistance", value=0),
+		TAG_Byte(name="OnGround", value=1),
+		TAG_Byte(name="Sleeping", value=0),
+		TAG_Short(name="SleepTimer", value=0)
+	])
+	
+	inventory = TAG_List(name="Inventory", type=TAG_Compound)
+	player.tags.append(inventory)
+	
+	motion = TAG_List(name="Motion", type=TAG_Double)
+	motion.tags.extend([
+		TAG_Double(value=0.0),
+		TAG_Double(value=0.0),
+		TAG_Double(value=0.0)
+	])
+	player.tags.append(motion)
+	
+	position = TAG_List(name="Pos", type=TAG_Double)
+	position.tags.extend([
+		TAG_Double(value=spawn_x+0.5),
+		TAG_Double(value=2.8),
+		TAG_Double(value=spawn_z+0.5)
+	])
+	player.tags.append(position)
+	
+	rotation = TAG_List(name="Rotation", type=TAG_Float)
+	rotation.tags.extend([
+		TAG_Float(value=0.0),
+		TAG_Float(value=0.0)
+	])
+	player.tags.append(rotation)
+	
+	data.tags.append(player)
+	level.tags.append(data)
+	return level
 
 # Draw a circle of a given radius and center, in the block dictionary, using the given block type (defaults to stone)
 def draw_disk(center, diameter, blocks, block_id=1, fill_air=False):
